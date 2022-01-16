8c63d7c781339f2b42a5d20a638b4802f3c0c88a
Nober Tai <s0913768710@gmail.com>
2022-01-16 20:17:00 +0800

    Add Program Counter

---
+++ src/main/ProgramCounter.v
@@ -0,0 +1,25 @@
+module ProgramCounter(
+    input clk,
+    input rst,
+    input [31:0] pc_in,
+    input [31:0] pc_en,
+    output [31:0] pc_out,
+);
+
+
+reg pc;
+
+always@(posedge clk or negedge rst) begin
+    if(!rst) begin
+        pc <= 0;
+    end else begin
+        pc <= (pc_en) ? pc_in : pc;
+    end
+end
+
+assign pc_out = pc;
+
+
+
+
+endmodule
