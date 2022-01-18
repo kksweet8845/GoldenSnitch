6a66a10605cb5fd1c982e844924adc1a540652a7
Nober Tai <s0913768710@gmail.com>
2022-01-19 02:36:35 +0800

    Leave the prefetch controller as empty

--- src/rtl/gs_prefetch_controller.sv
+++ src/rtl/gs_prefetch_controller.sv
@@ -2,9 +2,8 @@ module GS_PrefetchController
     import gs_pkg::*
 (
     input   logic           clk,
-    input   logic           rst,
+    input   logic           rst,          
     input   logic [31:0]    instr_addr_i,
-
     output  logic [31:0]    instr_o,
 
     //* status control
@@ -17,53 +16,7 @@ module GS_PrefetchController
 );
 
 
-    pf_state_t pf_fsm_cs, pf_fsm_ns;
     
-    logic [31:0]    nxt_instr_addr;
-    logic           nxt_instr_valid;
-
-    logic [31:0]    pf_instr;
-    logic           pf_instr_valid;
-
-
-    always_comb begin
-        pf_fsm_ns = pf_fsm_cs;
-        if(!rst) begin
-            
-        end else begin
-            unique case(pf_fsm_cs)
-            INITIAL: begin
-                pf_fsm_ns = PENDING;
-            end
-            PENDING: begin
-                if(instr_valid_i) begin
-                    nxt_instr_addr = instr_addr_i;
-                    nxt_instr_valid = instr_valid_i;
-                end else begin
-                    nxt_instr_addr = instr_addr_i;
-                    nxt_instr_valid = instr_valid_i;
-                end
-                pf_fsm_ns = (instr_valid_i) ? FETCHING : PENDING;
-            end
-            FETCHING: begin
-
-
-
-
-            end
-            endcase
-        end
-    end
-
-
-
-
-
-
-
-
-
-
 
 
 
